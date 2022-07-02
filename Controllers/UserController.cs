2a204a43b1e4e20dc5d2a3e78591a98b6a564fe5
utshaav <utshaav583@gmail.com>
2022-07-02 10:32:16 +0545

    Added authorization in controllers

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ApiAuth.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiAuth.Controllers
 {
+    [AllowAnonymous]
     public class UserController : BaseController
     { 
         private readonly TokenService _tokenService;
